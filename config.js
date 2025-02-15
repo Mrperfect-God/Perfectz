e29557bedeb5d4765186035ec67929bc3d01128e
Mrperfect-God <itsmecoder2468@gmail.com>
2025-02-15 03:30:37 +0000

    Hi

---
+++ config.js
@@ -0,0 +1,17 @@
+
+
+
+global.owner = [
+  "9779863479066", //should start with country code
+  ""  //second number if available
+]
+
+
+let fs = require('fs')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(`Update ${__filename}`)
+delete require.cache[file]
+require(file) 
+})
